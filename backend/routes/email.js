53af8a328ee4ac1be3866942a617a553e5d8ce84
syedmasroorjan@outlook.com <syedmasroorjan@outlook.com>
2018-05-21 16:56:11 +0530

    sd_masroor

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -3,7 +3,18 @@ var router = express.Router();
 var sendMails = require('../models/emailModel');
 var passport = require('passport');
 var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 //const users = require('../model/email');
+
+var options = {
+    auth: {
+        api_user: 'sdmasroor',
+        api_key: 'Masroor@123'
+    }
+}
+    
+var mailer = nodemailer.createTransport(sgTransport(options));
+
 
 
  router.post('/create',(req,res,next)=>{
@@ -13,15 +24,6 @@ var nodemailer = require('nodemailer');
 		emailBody : req.body.emailBody,
 		  creation_dt: Date.now()
 	});
-	var output=`<p> You have  new email request from SD</p>
-			<h3>Contact Details</h3>
-			<ul>
-			<li>From: SD Masroor</li>
-			<li>to:${req.body.to}</li>
-			<li>body:${req.body.emailBody}</li>
-			</ul>
-			<h3>Note</h3>
-			<p>This is a test mail.</p>`;
 
 	create.save((err,item)=>{
 
@@ -31,72 +33,64 @@ var nodemailer = require('nodemailer');
 
 		}
 		else{
-			 let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'sdmasroor0290@gmail.com', // generated ethereal user
-            pass: 'adhoreuuu' // generated ethereal password
-        },
-        tls:{
-        	rejectUnauthorized:false
-        }
-    });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: req.body.from, // sender address
-        to: req.body.to, // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: req.body.emailBody, // plain text body
-       // html: output // html body
-    };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-       var messageId =  info.messageId;
-        
-        if(messageId !==''){
+    var email = {
+    to: [req.body.to],
+    from: 'sdmasroor',
+    subject: 'Hi there',
+    text: req.body.emailBody,
+   // html: '<b>Awesome sauce</b>'
+};
+if(sendMail(email,item))
+{
+	console.log('#######');
+	res.json({msg:"meaage"});
+}
+ 
+
+}
+});
+	});
+
+function sendMail(email,item){
+var response=false;
+
+ mailer.sendMail(email, function(err, res) {
+    if (err) { 
+        console.log(err) 
+    }
+    console.log(res);
+            if(res.message =='success'){
         	//res.json(item._id);
         	sendMails.findOneAndUpdate({_id: item._id},{
 		$set:{
-			messageid: messageId,
+			//messageid: messageId,
 			messageDelivered : true,
 			
 		}
-	},
+	},{new:true},
 	function(err,result){
-
-		if(err){
-			res.json(err);
-
-		}
-		else{
-		res.json({msg :'Email Saved & sent successfully'});
-		}
+			// if(res.message =='success')
+			// { response =true;
+		// console.log(response);
+		//console.log(result);
+	//}
+			// if(err)
+			// 	{ response= false;
+			//  console.log(response);
+			// }
+			 return result;
 	}
 	);
         }
-        else{
-        	res.json({msg :'OOPS! somthing went wrong.'});
-        }
-    });
+        console.log(response);
+
+});
+
+}
 
 
-			
-		}
-	}
-	);
-
-
- });
 
 router.get('/getSentMails',(req,res,next)=>{
 
